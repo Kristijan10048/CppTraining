e13b7a9d9b63fcc111cc90da7c05eab959e84c21
kris <kris@kris-vb>
2018-01-03 00:03:03 +0100

    Initial implementation of Marcros in cpp

---
+++ MacrosJustDont/Macros.cpp
@@ -0,0 +1,23 @@
+/*
+ * Macros.cpp
+ *
+ *  Created on: Jan 2, 2018
+ *      Author: kris
+ */
+#include <iostream>
+using namespace std;
+
+//When the preprocessor encounters this directive, it replaces any occurrence of identifier in the rest of the code by replacement.
+//This replacement can be an expression, a statement, a block or simply anything. The preprocessor does not understand C++ proper,
+//it simply replaces any occurrence of identifier by replacement.
+
+//define max value for char buffer
+#define MAX_BUFFER_LENGTH 128
+#define
+
+int main()
+{
+	char buffer[MAX_BUFFER_LENGTH];
+
+	return 0;
+}
