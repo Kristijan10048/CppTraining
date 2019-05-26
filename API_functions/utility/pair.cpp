3b3f1756c46c308cbbc1e424408bda0f7806bb08
Kris10048 <kristijan.petkov@gmail.com>
2019-05-26 10:17:48 +0200

    simple example of pair class usage. Example with dynamic memory allocation

---
+++ API_functions/utility/pair.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include <utility>
+
+using namespace std;
+
+void main()
+{
+	std::pair<int, int> myPair;
+	myPair.first =10;
+	myPair.second = -15;
+	
+	cout<<"Pair ("<<myPair.first<<", "<<myPair.second<<")"<<endl;
+	
+	//dynamicly create pair object
+	std::pair<int, int> *pPair = new std::pair<int, int>(1,2);
+	
+	//accces members
+	cout<<"Dynamic pair:"<< pPair->first<<", "<<pPair->second<<endl;
+	
+	//delete object
+	delete pPair;
+}
