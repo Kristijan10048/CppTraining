34675de07bc811a846ac7499594e7f696fbb9800
Kris10048 <kristijan.petkov@gmail.com>
2019-05-26 10:24:03 +0200

    added swap function

--- API_functions/utility/pair.cpp
+++ API_functions/utility/pair.cpp
@@ -18,6 +18,12 @@ void main()
 	//accces members
 	cout<<"Dynamic pair:"<< pPair->first<<", "<<pPair->second<<endl;
 	
+	//swap elements
+	pPair->swap(myPair);
+	
+	//print after swaping
+	cout<<"Dynamic pair after swaping:"<< pPair->first<<", "<<pPair->second<<endl;
+	
 	//delete object
 	delete pPair;
 }
