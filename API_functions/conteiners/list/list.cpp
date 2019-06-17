61985ae79958f9e7c8e96c69c848e1289f824266
Kris10048 <kristijan.petkov@gmail.com>
2019-06-17 20:03:05 +0200

    initial list implementation

---
+++ API_functions/conteiners/list/list.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <list>
+
+using namespace std;
+
+//const keyword means that the function is not changing the argument
+void PrintList(const std::list<int> list)
+{
+	for(int it:list)
+		cout<<"Element :"<<it<<endl;
+}
+
+void main()
+{
+	std::list<int> lst = {2, 3, 5, 7};
+
+	//prints inital state of the list
+	PrintList(lst);
+	
+	lst.push_back(11);
+	lst.push_back(12);
+	
+	cout<<"List state after push_back call:"<<endl;
+	PrintList(lst);
+}
