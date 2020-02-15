d1b5581370ddc669d9ffade480963048353086d6
kristijan10048 <kristijan.petkov@gmail.com>
2020-02-15 13:55:04 +0100

    Initial commit

---
+++ TestBreakPoints/TestBreakPoints/TestBreakPoints.cpp
@@ -0,0 +1,11 @@
+// TestBreakPoints.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include "pch.h"
+
+int main()
+{
+	int b;
+	for (int i = 0 ; i < 10; i++)
+		b = i + 1;
+	
+}
