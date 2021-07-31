28bd20b6da9fc7b81b63fd386478201c9313d8e0
Kris10048 <kristijan.petkov@gmail.com>
2021-07-31 08:06:38 +0200

    Added some description

--- CppAsembly/MASAM_Test/MASAM_Test/Main.asm
+++ CppAsembly/MASAM_Test/MASAM_Test/Main.asm
@@ -10,12 +10,12 @@ myList DWORD 2, 3, 5, 8
 
 .code
 main PROC
-  mov eax, 0
-  add eax, 1
-  incr:
+  mov eax, 0 ;set eax to 0
+  add eax, 1 ; add 1 to eax
+  incrLoop:
   inc eax
   cmp eax, 9h
-  jne incr
+  jne incrLoop
   INVOKE ExitProcess, eax
 main ENDP
 
