9ec68db54e367420d0913efa19f3ecf19c965e81
Kris10048 <kristijan.petkov@gmail.com>
2021-08-07 09:11:21 +0200

    Clone of the original project

---
+++ CppAsembly/MASAM_Test-PrintString/MASAM_Test/Main.asm
@@ -0,0 +1,24 @@
+.386
+.386
+.model flat, stdcall
+.stack 4096
+
+ExitProcess PROTO, dwExitCode:DWORD
+
+.data
+myList DWORD 2, 3, 5, 8
+;The string to be printed 
+
+STRING DB 'This is a sample string', '$'
+.code
+main PROC
+  mov eax, 0 ;set eax to 0
+  add eax, 1 ; add 1 to eax
+  incrLoop:
+  inc eax
+  cmp eax, 9h
+  jne incrLoop
+  INVOKE ExitProcess, eax
+main ENDP
+
+END main        ;specify the program's entry point
